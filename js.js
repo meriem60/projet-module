b5e66fcc8173a61d1839f35ed5607d0e693ce577
meriem ben salem <117111853+meriem60@users.noreply.github.com>
2022-12-11 12:42:55 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,19 @@
+
+
+let menu = document.getElementById("wrap");
+
+function heart(){
+    menu.classList.toggle("openmenu");
+}
+
+let wrap = document.getElementById("menuwrap");
+
+function shop(){
+    wrap.classList.toggle("openmenu");
+}
+
+let submenu = document.getElementById("submenu");
+
+function user(){
+submenu.classList.toggle("open-menu");
+}
